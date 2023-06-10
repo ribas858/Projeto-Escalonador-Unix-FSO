f263e1a332a7a07cb24e8587b1e1a3b447552279
ribas858 <ribas858@gmail.com>
2023-06-10 04:06:49 -0300

    testando arquivos, e lista encadeada

--- t1_fso.c
+++ t1_fso.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<string.h>
 #include<stdlib.h>
 
 #include<sys/types.h>
@@ -10,85 +11,160 @@
 #include<sys/shm.h>
 
 
+typedef struct no {
+    int id;
+    char *nome;
+    int estado;         // 0 == Pronto, 1 == Terminou de executar
+    pid_t donoProcesso; // dono == PID, sem dono == -1
+    struct no *prox;
+} processo;
+
+void insereProcesso(processo **lista, int id, char *str) {
+    processo *novoProcesso = malloc(sizeof(processo));
+    if(novoProcesso) {
+        novoProcesso->id = id;
+
+        novoProcesso->nome = malloc(sizeof(char) * 20);
+        strcpy(novoProcesso->nome, str);
+
+        novoProcesso->estado = 0;
+        novoProcesso->donoProcesso = -1;
+        novoProcesso->prox = NULL;
+
+        if (*lista == NULL){
+            *lista = novoProcesso;
+        } else {
+            processo *aux = malloc(sizeof(processo));
+            aux = *lista;
+            while (aux->prox) {
+                aux = aux->prox;
+            }
+            aux->prox = novoProcesso;
+        }
+
+    } else {
+        printf("Erro ao inserir processo\n");
+    }
+}
+
+void printProcessos(processo *lista) {
+    while (lista) {
+        printf("\nId: %d\n", lista->id);
+        printf("Nome: %s\n", lista->nome);
+        printf("Estado: %d\n", lista->estado);
+        printf("Dono Processo: %d\n", lista->donoProcesso);
+        
+        lista = lista->prox;
+    }
+}
+
 int main() {
 
-    pid_t p_auxs[2];
-    int status0 = -1;
-    int status1 = -1;
+    char *read = malloc(sizeof(char) * 20);
+    char nome[20];
+
+    FILE *arqProcessos;
+    processo *listaProcessos = NULL;
     
+    arqProcessos = fopen("processos.txt", "r");
+    
+    if (arqProcessos == NULL) {
+        printf("ERRO! O arquivo de processos não foi aberto!\n");
 
-    int memoA = shmget(IPC_PRIVATE, sizeof(int), SHM_R | SHM_W | IPC_CREAT);
+    } else {
 
-    int *a = (int *) shmat(memoA, NULL, 0);
+        int i = 0;
+        while(fgets(read, 20, arqProcessos) != NULL) {
+            int tam = strlen(read);
+            read[tam-1] = '\0';
+            insereProcesso(&listaProcessos, i, read);
+            i++;
+        }
+        fclose(arqProcessos);
 
-    *a = 1;
+        printProcessos(listaProcessos);
 
-    for(int i=0; i<2; i++) {
-        p_auxs[i] = fork();
+        
+        
+        
+        // pid_t p_auxs[2];
+        // int status0 = -1;
+        // int status1 = -1;
         
 
-        if (p_auxs[i] == 0) {
-            //printf("FILHO(%d), meu pai é: %d\n", getpid(), getppid());
+        // int memoA = shmget(IPC_PRIVATE, sizeof(int), SHM_R | SHM_W | IPC_CREAT);
 
-            a = (int *) shmat(memoA, NULL, 0);
+        // int *a = (int *) shmat(memoA, NULL, 0);
+
+        // *a = 1;
+
+        // for(int i=0; i<2; i++) {
+        //     p_auxs[i] = fork();
             
-            
-            switch (i) {
-                case 0:
-                        
-                        printf("A: %d || Sou filho(%d) pid:%d    PAI(%d)\n", *a,  i, getpid(), getppid());
-                        
-                        
-                        while (*a != 50);
-                        
-                        printf("\nPassou...\n");
-                        shmdt(a);
-                        _exit(i);
-                        
-                        break;
+
+        //     if (p_auxs[i] == 0) {
+        //         //printf("FILHO(%d), meu pai é: %d\n", getpid(), getppid());
+
+        //         a = (int *) shmat(memoA, NULL, 0);
                 
-                case 1:
-                        
-                        printf("B: %d || Sou filho(%d) pid:%d    PAI(%d)\n", *a,  i, getpid(), getppid());
-                        sleep(20);
-                        *a = 50;
-                        shmdt(a);
-                        _exit(i);
-                        break;
-                default:
-                        break;
-            }
-        }
+                
+        //         switch (i) {
+        //             case 0:
+                            
+        //                     printf("A: %d || Sou filho(%d) pid:%d    PAI(%d)\n", *a,  i, getpid(), getppid());
+                            
+                            
+        //                     while (*a != 50);
+                            
+        //                     printf("\nPassou...\n");
+        //                     shmdt(a);
+        //                     _exit(i);
+                            
+        //                     break;
+                    
+        //             case 1:
+                            
+        //                     printf("B: %d || Sou filho(%d) pid:%d    PAI(%d)\n", *a,  i, getpid(), getppid());
+        //                     sleep(20);
+        //                     *a = 50;
+        //                     shmdt(a);
+        //                     _exit(i);
+        //                     break;
+        //             default:
+        //                     break;
+        //         }
+        //     }
 
-    }
+        // }
 
-    printf("PAI(%d), meu filho são: %d e %d\n\n", getpid(), p_auxs[0], p_auxs[1]);
-    
-
-    for(int i=0; i<2; i++) {
-        switch (i) {
-                case 0:
-                        wait(&status0);
-                        if(WIFEXITED(status0)) {
-                            printf("\nMeu filho(%d) pid: %d , Morreu..\n\n", WEXITSTATUS(status0), p_auxs[i]);
-                        }
-                        break;
-
-                case 1:
-                        wait(&status1);
-                        if(WIFEXITED(status1)) {
-                            printf("Meu filho(%d) pid: %d , Morreu..\n\n", WEXITSTATUS(status1), p_auxs[i]);
-                        }
-                        break;
-                default:
-                        break;
-        }
-
+        // printf("PAI(%d), meu filho são: %d e %d\n\n", getpid(), p_auxs[0], p_auxs[1]);
         
-    }
 
-    shmdt(a);
-    shmctl(memoA, IPC_RMID, NULL);
+        // for(int i=0; i<2; i++) {
+        //     switch (i) {
+        //             case 0:
+        //                     wait(&status0);
+        //                     if(WIFEXITED(status0)) {
+        //                         printf("\nMeu filho(%d) pid: %d , Morreu..\n\n", WEXITSTATUS(status0), p_auxs[i]);
+        //                     }
+        //                     break;
+
+        //             case 1:
+        //                     wait(&status1);
+        //                     if(WIFEXITED(status1)) {
+        //                         printf("Meu filho(%d) pid: %d , Morreu..\n\n", WEXITSTATUS(status1), p_auxs[i]);
+        //                     }
+        //                     break;
+        //             default:
+        //                     break;
+        //     }
+
+            
+        // }
+
+        // shmdt(a);
+        // shmctl(memoA, IPC_RMID, NULL);
+    }
 
     return 0;
 }
